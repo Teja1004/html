07b26e1c7454f7641623bfa8ff00407fcb4dc131
Teja1004 <tejabattina850@gmail.com>
2023-05-10 17:10:37 +0530

    new js file is added

---
+++ javascript/funtions.js
@@ -0,0 +1,20 @@
+function add(x,y){
+    let z=x+y;
+    return(z);
+}
+let result=add(14,24);
+console.log(result);
+let d=add(56,55);
+    console.log(d);
+
+    function max(x,y){
+        let maxval=0;
+        if(x<y){
+            maxval=y;
+            console.log(maxval);
+        }else{
+            maxval=x;
+            console.log(maxval);
+        }
+    }
+    let t=max(12,4);
